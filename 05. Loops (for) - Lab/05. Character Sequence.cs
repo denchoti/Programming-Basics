1d24cacf50457da7fba101d131f0b56b3f13df50
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 19:10:37 +0200

    Create 05. Character Sequence.cs

---
+++ 05. Loops (for) - Lab/05. Character Sequence.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _07.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
