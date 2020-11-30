01501d2740548c02e216ca11690bf5b7c3b33f10
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 19:12:41 +0200

    Create 06. Vowels Sum.cs

---
+++ 05. Loops (for) - Lab/06. Vowels Sum.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace _06.VowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = Console.ReadLine();
+            var sum = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == 'a')
+                {
+                    sum += 1;
+                }
+                else if (word[i] == 'e')
+                {
+                    sum += 2;
+                }
+                else if (word[i] == 'i')
+                {
+                    sum += 3;
+                }
+                else if (word[i] == 'o')
+                {
+                    sum += 4;
+                }
+                else if (word[i] == 'u')
+                {
+                    sum += 5;
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
