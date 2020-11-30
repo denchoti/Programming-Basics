6fc339f8f356543a7d57b0824bb968d69a64c368
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 18:12:07 +0200

    Create 04. Rectangle Area.cs

---
+++ 01.First Steps In Coding/04. Rectangle Area.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ExpressionAndReactangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+
+            int area = a * b;
+            Console.WriteLine(area);
+
+        }
+    }
+}
