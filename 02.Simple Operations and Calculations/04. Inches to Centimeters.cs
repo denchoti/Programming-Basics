a855caf34d4b2ebc3a4c3f124d2c5226dcc388f3
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 18:23:11 +0200

    Create 04. Inches to Centimeters.cs

---
+++ 02.Simple Operations and Calculations/04. Inches to Centimeters.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace InchesToCm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inches = ");
+            var inches = double.Parse(Console.ReadLine());
+            var centimeters = inches * 2.54;
+            Console.Write("Centimeters = ");
+            Console.WriteLine(centimeters);
+        }
+    }
+}
