2264b5b9e2b18fa5d9b7dfd03d8563512835d74f
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 20:00:34 +0200

    Create 06. Special Numbers.cs

---
+++ 07.  Nested Loops - Exercise/06. Special Numbers.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace _5.PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int number = 1111; number < 9999; number++)
+            {
+                int thousands = number / 1000;
+                int hundreds = (number / 100) % 10;
+                int tens = (number / 10) % 10;
+                int units = number % 10;
+
+                bool check1 = thousands != 0 && n % thousands == 0;
+                bool check2 = hundreds != 0 && n % hundreds == 0;
+                bool check3 = tens != 0 && n % tens == 0;
+                bool check4 = units != 0 && n % units == 0;
+
+                if (check1 && check2 && check3 && check4)
+                {
+                    Console.Write(number + " ");
+                }
+
+
+
+            }
+        }
+    }
+}
