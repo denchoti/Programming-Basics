b9150dfa46fce70813588de3f5f699382a3aee31
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 19:36:59 +0200

    Create 04. Walking.cs

---
+++ 06. Loops (while) - Exercise/04. Walking.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace _14.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 0;
+
+            while (steps < 10000)
+            {
+                
+                string command = Console.ReadLine();
+               
+                if (command == "Going home")
+                {
+                    int stepsToHome = int.Parse(Console.ReadLine());
+                    steps += stepsToHome;
+                    break;
+
+                }
+                else
+                {
+                    
+                    int walkedSteps = int.Parse(command);
+                    steps += walkedSteps;
+                }
+            }
+
+            if (steps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - steps} more steps to reach goal.");
+            }
+        }
+    }
+}
