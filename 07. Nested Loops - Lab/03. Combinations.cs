9d4cdc6f4326468adde39080816d820bf7344b3a
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 19:55:55 +0200

    Create 03. Combinations.cs

---
+++ 07. Nested Loops - Lab/03. Combinations.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace _03.Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int validCombinationsCount = 0;
+
+            for (int x1 = 0; x1 <= number; x1++)
+            {
+                for (int x2 = 0; x2 <= number; x2++)
+                {
+                    for (int x3 = 0; x3 <= number; x3++)
+                    {
+                        if (x1 + x2 + x3 == number)
+                        {
+                            validCombinationsCount++;
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validCombinationsCount);
+        }
+    }
+}
