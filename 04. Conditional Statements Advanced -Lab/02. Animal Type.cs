0eb0abdf83b9676fd39eb9afa3ff0a583b57d0e2
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 18:53:11 +0200

    Create 02. Animal Type.cs

---
+++ 04. Conditional Statements Advanced -Lab/02. Animal Type.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace _02.AnimalType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "snake":
+                case "crocodile":
+                case "tortoise":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
